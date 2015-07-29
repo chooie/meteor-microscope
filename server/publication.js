eb78c96d9f0606f32c463bc53346a6d2d1bb13ee
Charlie Hebert <charlie.hebert92@gmail.com>
2015-07-29 22:05:01 +0100

    Augment the postList route to take a limit

--- server/publication.js
+++ server/publication.js
@@ -1,5 +1,11 @@
 Meteor.publish('posts', function() {
-  return Posts.find();
+  Meteor.publish('posts', function(options) {
+    check(options, {
+      sort: Object,
+      limit: Number
+    });
+    return Posts.find({}, options);
+  });
 });
 
 Meteor.publish('comments', function(postId) {
